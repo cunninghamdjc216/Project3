725d93ada5154134c72f1ead32fef6f24655f765
celenacunningh1 <18008661-celenacunningh1@users.noreply.replit.com>
2023-04-18 13:16:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let stayInBed=document.querySelector(".option-one");
+let goJoinYourClan=document.querySelector(".option-two");
+let storyOpening=document.querySelector(".story-opening");
+let optionOneScreen=document.querySelectorAll(".option-one-screen");
+let optionTwoScreen=document.querySelectorAll(".option-two-screen");
+let optionOneEnd=document.querySelectorAll(".option-one-end");
+let optionTwoEnd=document.querySelectorAll(".option-two-end");
+
+
+stayInBed.onclick=function(){
+storyOpening.style.display= "none";
+optionOneScreen.style.display="block";
+  optionTwoScreen.style.display="none"; 
+  
+};
+
+goJoinYourClan.onclick=function(){
+storyOpening.style.display= "none";
+  storyOpening.style.display= "none";
+  optionTwoScreen.style.display="none";
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
